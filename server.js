d76d12ab54e7c6d1de69feccce3927077498550d
Vishal-wankhade <87899397+Vishal-wankhade@users.noreply.github.com>
2023-06-04 01:43:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,16 +2,19 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const fs = require("fs");
-const port = process.env.PORT || 5001
-
+const port = process.env.PORT || 5003;
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
+const jsonfile = require('jsonfile');
+
+const dbPath = path.join(__dirname, "db.json");
 
 let likeCount = 0;
 
-// Load the like count from the file if it exists
-if (fs.existsSync("count.txt")) {
-  likeCount = parseInt(fs.readFileSync("count.txt", "utf8"));
+// Load the like count from the database if it exists
+if (fs.existsSync(dbPath)) {
+  const data = jsonfile.readFileSync(dbPath);
+  likeCount = data.likeCount || 0;
 }
 
 app.use(express.static('public'));
@@ -20,13 +23,12 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname,"public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
-
 
 app.get("/like", (req, res) => {
   likeCount++;
-  updateLikeCountFile();
+  updateLikeCount();
   res.json({ count: likeCount });
 });
 
@@ -34,15 +36,11 @@ app.get("/count", (req, res) => {
   res.json({ count: likeCount });
 });
 
-function updateLikeCountFile() {
-  fs.writeFileSync("count.txt", likeCount.toString());
+function updateLikeCount() {
+  const data = { likeCount };
+  jsonfile.writeFileSync(dbPath, data);
 }
 
-
-
-
-  
-
 app.listen(port, () => {
-  console.log("Server listening on port "+ port);
+  console.log("Server listening on port " + port);
 });
